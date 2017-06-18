2366df20c4d5282bda1cd99dd8e4285ac48fe5e8
levinjimmy <928314021@qq.com>
2017-06-18 10:52:31 +0800

    v20170618 使用gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,5 @@
-python ./manage.py runserver 8000
+./manage.py makemigrations
+echo "make migraions"
+./manage.py migrate
+echo "migrate"
+/usr/local/bin/gunicorn edu.wsgi:application -w 2 -b :8000
--- edu/settings.py
+++ edu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'CEdu',
+    'gunicorn',
 ]
 
 MIDDLEWARE_CLASSES = [
