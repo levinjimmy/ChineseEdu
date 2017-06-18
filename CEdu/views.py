1636613c229ad0262b1996cc75e15b9a58c99084
levinjimmy <928314021@qq.com>
2017-06-18 20:55:57 +0800

    v20170618 添加模板

--- CEdu/views.py
+++ CEdu/views.py
@@ -1,3 +1,7 @@
+#coding=utf-8
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseRedirect,JsonResponse
+from django.core.urlresolvers import reverse
+def index(req):
+	return render(req,'index.html')
 # Create your views here.
--- edu/urls.py
+++ edu/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-
+from CEdu import urls as CEdu_urls
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^cedu/',include(CEdu_urls,namespace='cedu')),
 ]
